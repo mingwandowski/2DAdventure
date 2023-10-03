7e943b65e342b8b88a2a7df08be25a66cc0fc6a3
haoming02 <haoming2019@gmail.com>
2023-10-02 20:19:33 -0400

    Implement player movement and flip

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,10 +6,15 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
     public Vector2 inputDirection;
+    public float speed = 200f;
+
+    private int faceDir = 1;
 
     private void Awake() {
         inputControl = new PlayerInputControl(); 
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable() {
@@ -22,5 +27,16 @@ public class PlayerController : MonoBehaviour
 
     private void Update() {
         inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate() {
+        Move();
+        Debug.Log(123);
+    }
+
+    private void Move() {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+        faceDir = inputDirection.x == 0 ? faceDir : inputDirection.x > 0 ? 1 : -1;
+        transform.localScale = new Vector3(faceDir, 1, 1);
     }
 }
