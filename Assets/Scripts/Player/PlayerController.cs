0016960c9c2e8be5e8760ea9889b52709cabe20f
haoming02 <haoming2019@gmail.com>
2023-10-03 11:00:07 -0400

    Add jump feature

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,16 @@ public class PlayerController : MonoBehaviour
     public PlayerInputControl inputControl;
     private Rigidbody2D rb;
     public Vector2 inputDirection;
+    [Header("Movement")]
     public float speed = 200f;
+    public float jumpForce = 5f;
 
     private int faceDir = 1;
 
     private void Awake() {
-        inputControl = new PlayerInputControl(); 
         rb = GetComponent<Rigidbody2D>();
+        inputControl = new PlayerInputControl();
+        inputControl.Gameplay.Jump.started += Jump;
     }
 
     private void OnEnable() {
@@ -30,13 +34,17 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        Move();
-        Debug.Log(123);
+        Move(); 
     }
 
     private void Move() {
         rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
         faceDir = inputDirection.x == 0 ? faceDir : inputDirection.x > 0 ? 1 : -1;
         transform.localScale = new Vector3(faceDir, 1, 1);
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 }
